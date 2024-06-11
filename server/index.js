f0767a6c8edb05826ee9dc3940dded5e15aa112d
Vadim Hevdi <vadimhevdi@MacBook-Pro-Vadim.local>
2024-06-11 19:33:23 +0300

    udate url

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', userRoutes);
+app.use('/', userRoutes);
 app.get('/', (req, res) => {
   res.send('Start');
 });
