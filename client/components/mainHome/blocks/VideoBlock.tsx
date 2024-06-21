1143d2ac480fea9b5dca960acb3a774293372dca
Vadim Hevdi <vadimhevdi@MacBook-Pro-Vadim.local>
2024-06-21 19:40:59 +0300

    add auto play video

--- client/components/mainHome/blocks/VideoBlock.tsx
+++ client/components/mainHome/blocks/VideoBlock.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function VideoBlock() {
+  const videoRef = useRef(null);
   const [scale, setScale] = useState(1);
 
   const handleScroll = () => {
@@ -9,13 +10,26 @@ export default function VideoBlock() {
     const blockTop = (document.getElementById('scale-block')?.offsetTop || 0) - 300;
 
     if (!blockTop) return;
+
     if (scrollY >= blockTop - windowHeight && scrollY <= blockTop) {
       const progress = (scrollY - (blockTop - windowHeight)) / windowHeight;
       setScale(1 + progress);
     } else if (scrollY > blockTop) {
       setScale(2);
+      if (videoRef.current) {
+        (videoRef.current as any).play();
+      }
     } else {
       setScale(1);
+      if (videoRef.current) {
+        (videoRef.current as any).pause();
+      }
+    }
+
+    if (scrollY > blockTop + windowHeight) {
+      if (videoRef.current) {
+        (videoRef.current as any).pause();
+      }
     }
   };
 
@@ -27,23 +41,31 @@ export default function VideoBlock() {
   }, []);
 
   return (
-
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div
         id="scale-block"
         className="transition-transform duration-200 ease-out w-1/2 h-[520px] mt-10"
         style={{ transform: `scale(${scale})` }}
       >
-        <iframe
+        <video
+          ref={videoRef}
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/zhPayDylYSc?si=69DV1i1QmPtUxULh"
-          title="YouTube video player" frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-        ></iframe>
+          muted // Ensure the video is muted for autoplay
+          controls={false}
+        >
+          <source src="/hero/video.mp4" type="video/mp4"/>
+        </video>
+        {/*<iframe*/}
+        {/*  width="100%"*/}
+        {/*  height="100%"*/}
+        {/*  src="https://www.youtube.com/embed/zhPayDylYSc?si=69DV1i1QmPtUxULh"*/}
+        {/*  title="YouTube video player" frameBorder="0"*/}
+        {/*  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
+        {/*  referrerPolicy="strict-origin-when-cross-origin" allowFullScreen*/}
+        {/*  style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}*/}
+        {/*></iframe>*/}
       </div>
     </div>
-  )
+  );
 }
