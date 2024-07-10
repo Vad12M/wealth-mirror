3989ed1d28705c0df37346e832657de94dc014bc
Vadim Hevdi <vadimhevdi@MacBook-Pro-Vadim.local>
2024-07-11 00:31:46 +0300

    change response

--- server/controllers/CarController.js
+++ server/controllers/CarController.js
@@ -27,10 +27,8 @@ export const updateCar = async (req, res) => {
       });
     }
 
-    await CarModel.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).json({
-      id: req.params.id,
-    });
+    const updatedCar = await CarModel.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json(updatedCar);
   } catch (error) {
     res.status(500).json({
       message: error.message
