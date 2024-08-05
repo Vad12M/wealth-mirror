656ceda07dd4a5942c59b6472134091720c91e86
Vadim Hevdi <vadimhevdi@MacBook-Pro-Vadim.local>
2024-08-06 01:40:07 +0300

    update arrows

--- client/components/layouts/AuthLayout.tsx
+++ client/components/layouts/AuthLayout.tsx
@@ -115,20 +115,41 @@ export default function AuthLayout({ children, type }: {
             ))}
           </Swiper>
         </div>}
-      {type === 'register' && <div className="flex items-center space-x-8 absolute right-36 bottom-[135px] z-50 group">
+      {type === 'register' && <div className="flex items-center space-x-8 absolute right-36 bottom-[135px] z-50">
         <Button
           typeButton="none"
           className="p-5 rounded-full border border-white hover:bg-white transition duration-600 group"
           onClick={handlePrev}
         >
-          <ArrowLeft/>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            className="group-hover:stroke-black stroke-white transition duration-600"
+          >
+            <path d="M19 12H5M5 12L12 19M5 12L12 5" strokeWidth="2" strokeLinecap="round"
+                  strokeLinejoin="round"/>
+          </svg>
         </Button>
         <Button
           typeButton="none"
           className="p-5 rounded-full border border-white hover:bg-white transition duration-600 group"
           onClick={handleNext}
         >
-          <ArrowRight/>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            className="group-hover:stroke-black stroke-white transition duration-600"
+          >
+            <path
+              d="M5 12H19M19 12L12 5M19 12L12 19" strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round"/>
+          </svg>
         </Button>
       </div>}
       <div className="absolute w-full z-50 bg-naturalBlack bottom-0 md:block hidden">
